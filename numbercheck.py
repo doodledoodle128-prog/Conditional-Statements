cf285485c62cad2022aaa2110971e00028e915d1
doodledoodle128-prog <doodle.doodle128@gmail.com>
2026-03-03 15:53:06 -0500

    Add files via upload

---
+++ numbercheck.py
@@ -0,0 +1,9 @@
+i = int(input("What is your number?: "))
+
+if (i > 15):
+    print(i,"is greater than 15")
+    print("im in if block")
+else:
+    print(i,'is less than 15')
+    print("Im in else block")
+print('im in no block')
